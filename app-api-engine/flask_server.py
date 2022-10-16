d0d2d671dc58919cde1753c3cc1e6b2e9ffa637a
Jhanvi Girish <jhanvigirish@utexas.edu>
2022-10-16 14:35:59 -0500

    updated react app files + flask file: correctly hits server

--- app-api-engine/flask_server.py
+++ app-api-engine/flask_server.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 
 @app.route('/validateCredentials')
 def validate_credentials():
+    ## pass the username and password (which is in a post body)
+    ## check to see if its in the data
     return jsonify('test - hit the server!!')
 
 
