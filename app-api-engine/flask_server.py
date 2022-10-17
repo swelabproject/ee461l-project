8a9c40eb9b61ff2c7555472519e585aa7a14b0d3
agallardo5921 <andresgallardo2001@gmail.com>
2022-10-17 13:24:16 -0500

    Create_User Page

--- app-api-engine/flask_server.py
+++ app-api-engine/flask_server.py
@@ -8,5 +8,11 @@ def validate_credentials():
     return jsonify('test - hit the server!!')
 
 
+@app.route('/createNewUser')
+def create_new_user():
+
+    return jsonify('test - hit the server!!')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
