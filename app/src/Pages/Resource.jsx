a84edb86deae49bcaa7ac840eef407a67b0e4142
vicsaa01 <112038482+vicsaa01@users.noreply.github.com>
2022-11-07 20:25:33 -0600

    Updated Resource.jsx

--- app/src/Pages/Resource.jsx
+++ app/src/Pages/Resource.jsx
@@ -3,20 +3,20 @@ import Button from '@mui/material/Button';
 import logo from './logo.svg';
 import './stylesheet.css';
 
-// further in the project, these values will be taken from the MongoDB database, and updated accordingly
-const projectId = "Project name";
-const hw1_ava = 100;
-const hw2_ava = 100;
+// taken from prev page
+const userId = "USER A";
+const projectId = "PROJECT X";
+var hw1_ava;
+var hw2_ava;
 
 function checkIn_hardware1(){
     fetch('/manageproject/in1/' + projectId + '&' + document.getElementById("input1").value + '&' + hw1_ava)
       .then(function (response) {
           return response.json();
       }).then(function (text) {
-          //increase in database
           hw1_ava = hw1_ava + text.qty;
           alert(text.qty + " hardware sets checked in by " + text.projectID + " in set 1.");
-          alert("new availability = " + hw1_ava);
+          window.location.reload();
       });
 }
 
@@ -25,10 +25,9 @@ function checkIn_hardware2(){
       .then(function (response) {
           return response.json();
       }).then(function (text) {
-          //increase in database
           hw2_ava = hw2_ava + text.qty;
           alert(text.qty + " hardware sets checked in by " + text.projectID + " in set 2.");
-          alert("new availability = " + hw2_ava);
+          window.location.reload();
       });
 }
 
@@ -37,10 +36,9 @@ function checkOut_hardware1(){
       .then(function (response) {
           return response.json();
       }).then(function (text) {
-          //decrease in database
           hw1_ava = hw1_ava - text.qty;
           alert(text.qty + " hardware sets checked out by " + text.projectID + " in set 1.");
-          alert("new availability = " + hw1_ava);
+          window.location.reload();
       });
 }
 
@@ -49,10 +47,9 @@ function checkOut_hardware2(){
       .then(function (response) {
           return response.json();
       }).then(function (text) {
-          //decrease in database
           hw2_ava = hw2_ava - text.qty;
           alert(text.qty + " hardware sets checked out by " + text.projectID + " in set 2.");
-          alert("new availability = " + hw2_ava);
+          window.location.reload();
       });
 }
 
@@ -82,13 +79,13 @@ class Project extends React.Component {
       <div>
         <label> {this.props.name} </label>
         <p>
-          HWSet1: {this.props.av1}/100
+          <span id="set1"/>
           <input id="input1" type="text" placeholder="Quantity"/>
           <CheckInButton set="1"/>
           <CheckOutButton set="1"/>
         </p>
         <p>
-          HWSet2: {this.props.av2}/100
+          <span id="set2"/>
           <input id="input2" type="text" placeholder="Quantity"/>
           <CheckInButton set="2"/>
           <CheckOutButton set="2"/>
@@ -99,6 +96,15 @@ class Project extends React.Component {
 }
 
 function App() {
+  fetch('/manageproject')
+      .then(function (response) {
+          return response.json();
+      }).then(function (text) {
+          hw1_ava = text.Ava1;
+          document.getElementById('set1').innerHTML = 'HWSet1: ' + hw1_ava + '/100 ';
+          hw2_ava = text.Ava2;
+          document.getElementById('set2').innerHTML = 'HWSet2: ' + hw2_ava + '/100 ';
+      });
   return (
     <div className="login-container">
       <h1> Manage project </h1>
