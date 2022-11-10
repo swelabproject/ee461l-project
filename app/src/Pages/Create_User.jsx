c910b015bc07489f49bc257dfff368959d5387a7
Zach Yeo <zyeo@users.noreply.github.com>
2022-11-10 16:04:19 -0600

    add projects to users db

--- app/src/Pages/Create_User.jsx
+++ app/src/Pages/Create_User.jsx
@@ -4,64 +4,35 @@ import './stylesheet.css';
 export const Create_User = (props) => {
     const [username, setUserName] = useState('')
     const [password, setPassword] = useState('')
-    const [name, setName] = useState('')
+    const [id, setID] = useState('')
 
-    // const handleSubmit = (e) => {
-    //     // call API validateCredentials
-    //     fetch("/createNewUser").then(
-    //         res => res.json()
-    //     ).then(
-    //         message => {
-    //             console.log(message)
-    //         }
-    //     )
-    //     e.preventDefault();
-    //     console.log(username + " " + password);
-    //     //authorized(e)
-    // }
-
-    //added from jhanvis
     const handleSubmit = (e) => {
         e.preventDefault()
-        // call API validateCredentials
+        console.log("handle submit")
         const reqOptions = {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({'user': username, 'password': password})
+            body: JSON.stringify({'user': username, 'password': password, 'UserID': id})
         }
         try {
-            fetch("/validateCredentials", reqOptions).then(
+            fetch("/createNewUser", reqOptions).then(
                 res => res.json()
             ).then((data) => {
                     console.log(data)
-                    if (data.validation === "valid") {
-                        console.log("found user and password")
-                        authorized(e)
+                    if (data.existing === "true") {
+                        console.log("Error: User already exists")
+                        alert('Error: User already exists\ntry another userID')
                     } else {
-                        console.log("DIDNT FIND user and password")
-                       invalidCredentials(e)
+                        console.log("Created New User")
+                        props.onPageUpdate('login')
                     }
                 })
 
         } catch (e) {
             console.log(e)
         }
-    }
-
-    //added from jhanvis
-    const authorized = (e) => {
-        // we can access handleLogin from App since it was passed as a prop
-        console.log("changing the page to user home page")
-        props.onPageUpdate('user-home')
-    }
-
-    //added from jhanvis
-    const invalidCredentials = (e) => {
-        // we can access handleLogin from App since it was passed as a prop
-        alert('Invalid credentials. Try again, or create a new account.')
-        props.onPageUpdate('login')
     }
 
     return (
@@ -84,14 +55,13 @@ export const Create_User = (props) => {
                        id="password"
                        name="password"></input>
 
-                <label htmlFor="New Name">New Name</label>
-                <input value={name}
-                       onChange={(e) => setName(e.target.value)}
-                       type="name"
-                       placeholder="Name"
-                       id="name"
-                       name="name"></input>
-
+                <label htmlFor="New ID">New ID</label>
+                <input value={id}
+                       onChange={(e) => setID(e.target.value)}
+                       type="id"
+                       placeholder="User ID"
+                       id="id"
+                       name="id"></input>
 
                 <button type="submit">Create New User</button>
 
